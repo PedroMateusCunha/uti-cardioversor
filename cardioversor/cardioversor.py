c63d476e1b647e48dfd0c653ef4684fa194b9353
rafaellopesbr <rafael.lopes.br@gmail.com>
2023-05-13 22:47:21 -0300

    Ajustes de correção de issues relatadas pelo Pylint e para uso de Git Actions (Docker, Pylint, Bandit)

--- cardioversor/cardioversor.py
+++ cardioversor/cardioversor.py
@@ -1,5 +1,12 @@
+"""Modulo para gerenciamento do cardioversor do leito hospitalar."""
 class Cardioversor:
+    """
+    Classe para gerenciamento do cardioversor do leito hospitalar
+    """
     def __init__(self):
+        """
+        Metodo para inicializar os atributos do cardioversor
+        """
         self.freq_cardiaca = 0
         self.identificador_marca_passo = False
         self.potencia = 0
@@ -7,18 +14,33 @@ class Cardioversor:
         self.ligado = False
 
     def set_freq_cardiaca(self, freq_cardiaca: int):
-            self.freq_cardiaca = freq_cardiaca
+        """
+        Metodo para setar o valor da frequencia cardiaca do paciente
+        """
+        self.freq_cardiaca = freq_cardiaca
 
     def set_identificador_marca_passo(self, identificador_marca_passo: bool):
+        """
+        Metodo para setar a existencia de marcapasso no paciente
+        """
         self.identificador_marca_passo = identificador_marca_passo
 
     def set_potencia(self, potencia: int):
+        """
+        Metodo para setar a potencia de atuação do cardioversor
+        """
         self.potencia = potencia
 
     def set_frequencia(self, frequencia: int):
+        """
+        Metodo para setar a frequencia de atuação do cardioversor
+        """
         self.frequencia = frequencia
 
     def ligar(self):
+        """
+        Metodo para ligar o cardioversor
+        """
         if self.identificador_marca_passo == False:
             self.ligado = True
             return {"message": "Cardioversor ligado com sucesso"}
@@ -26,9 +48,15 @@ class Cardioversor:
             return {"aviso": "Marca passo identificado"}
 
     def desligar(self):
+        """
+        Metodo para desligar o cardioversor
+        """
         self.ligado = False
     
     def get_status(self):
+        """
+        Metodo para recuperar o status do cardioversor
+        """
         return { "cardioversor": {
                 "freq_cardiaca": self.freq_cardiaca,
                 "identificador_marca_passo": self.identificador_marca_passo,
